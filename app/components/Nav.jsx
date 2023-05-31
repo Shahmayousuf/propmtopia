8f12240418361dddba258317f3624305bfced02c
Shahmayousuf <shahmayousuf@gmail.com>
2023-05-31 16:00:29 +0530

    add buttons

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -6,6 +6,8 @@ import {  useState, useEffect } from "react"
 import{signIn,signOut,useSession,getProviders}from 'next-auth/react'
 
 const Nav = () => {
+
+    const isUserLoggedIn=true
   return (
    <nav className="flex-between w-full mb-15 pt-3">
 <Link href="/" className="flex-gap-2 flex-center">
@@ -16,6 +18,20 @@ const Nav = () => {
      </Image>
      <p className="logo_text">Promptopia</p>
 </Link> 
+{/* desktop navigation */}
+       <div className="sm:flex hidden ">
+        {isUserLoggedIn?(
+            <div className="flex gap-3 md:gap-5">
+                <Link href='/create-prompt'className="black_btn">
+                    Create post
+                </Link>
+                <button type="button" onClick={signOut}className="outline_btn">SignOut</button>
+            </div>
+        ):(
+            <>
+            </>
+        )}
+       </div>
    </nav>
   )
 }
