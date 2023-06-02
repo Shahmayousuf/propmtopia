63d5def0d2bf34c5cb06e4fd2471a02b4e174628
Shahmayousuf <shahmayousuf@gmail.com>
2023-06-02 16:11:08 +0530

    user.jsx

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,28 @@
 import NextAuth from "next-auth/next";
-import  GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
+import { connectToDB } from "@utils/database";
+//  console.log( {clientId:'process.env.GOOGLE_ID',
+//  clientSecret:'process.env.GOOGLE_CLIENT_SECRET'});
 
- console.log( {clientId:'process.env.GOOGLE_ID',
- clientSecret:'process.env.GOOGLE_CLIENT_SECRET'});
- 
-const handler=NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId:'process.env.GOOGLE_ID',
-            clientSecret:'process.env.GOOGLE_CLIENT_SECRET'
-        })
-    ],
-    async session({session}){
-
-    },
-    async signIn({profile}){
-
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: "process.env.GOOGLE_ID",
+      clientSecret: "process.env.GOOGLE_CLIENT_SECRET",
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+      //check if a user already exists
+      //if not create a new user
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-}) 
-export {handler as GET,handler as POST}
+  },
+});
+export { handler as GET, handler as POST };
