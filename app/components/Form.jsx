76e037b979f225798aaa3d15d40d1695e8b8b40a
Shahmayousuf <shahmayousuf@gmail.com>
2023-06-09 11:34:00 +0530

    form

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -11,12 +11,12 @@ const Form = ({
   return (
     <div>
       <section className="w-full max-w-full flex-start flex-col">
-        <h1 className="head_text text-left"><span className="blue_gradient">{type} Post</span></h1>
+        <h1 className="head_text text-left w-full"><span className="blue_gradient">{type} Post</span></h1>
         <p className=" desc text-left max-w-md">{type} and share amazing prompts with the world,
         and let you imagination run wild with any AI-powered platform</p>
         <form 
         onSubmit={handleSubmit}
-        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
+        className="mt-10 w-full max-w-2xl flex flex-col gap-4 glassmorphism">
           <label><span className="font-satoshi font-semibold text-base text-grey-700">Your AI Prompt</span>
             <textarea
             value={post.prompt}
@@ -25,6 +25,22 @@ const Form = ({
             required
             className="form_textarea"></textarea>
           </label>
+          <label><span className="font-satoshi font-semibold text-base text-grey-700">tag(#product,#webdevelopment,#idea)</span>
+            <input
+            value={post.tag}
+            onChange={(e)=>setpost({...post,tag:e.target.value})}
+            placeholder="#tag"
+            required
+            className="form_input"></input>
+          </label>
+          <div className="f justify-end mx-3 mb-5 flex gap-3">
+             <Link href='/'className="px-5 py-2 text-grey-500 text-sm font-medium">Cancel</Link>
+             <button type='submit'
+             disabled={submitting}
+             className="px-5 py-2 text-sm bg-orange-500 rounded-full font-medium text-white hover:bg-orange-400 outline:none  ">
+              {submitting?`${type}...`:type}
+             </button>
+          </div>
         </form>
       </section>
     </div>
